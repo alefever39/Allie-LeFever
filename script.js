e9e38cc1938d9c864194b86cf108c46e38f250ae
Allie LeFever <alefever39@gmail.com>
2022-04-15 10:39:05 -0600

    added text to main page, added JS to reveal text, changed color

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const aboutMe = document.getElementById("about-me");
+const aboutMe = document.getElementById('about-me-main');
+const hobbies = document.getElementById('my-hobbies-main');
 
-function colorChange(elementId) {
+function selectText(elementId) {
+    aboutMe.style.display = "none"
+    hobbies.style.display = "none"
+
     const Id = document.getElementById(elementId);
-    Id.style.color = "red";
+    Id.style.display = "block";
 }
